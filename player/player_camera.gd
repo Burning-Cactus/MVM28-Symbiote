cfd884bff8a18437e0cd7592285c0cc4f90384ef
Burning Cactus <23108473+Burning-Cactus@users.noreply.github.com>
2025-05-23 22:07:26 -0500

    Add a camera that follows the player.

---
+++ player/player_camera.gd
@@ -0,0 +1,14 @@
+# Extended camera node to avoid loading a whole scene when a camera is needed.
+class_name PlayerCamera
+extends Camera2D
+
+@export var player: Node2D
+var target: Node2D
+
+func _ready() -> void:
+	assert(player != null, "Error: player must not be null!")
+	target = player
+
+# Follow the target.
+func _process(delta: float) -> void:
+	position = target.position
