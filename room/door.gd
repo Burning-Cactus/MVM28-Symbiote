9f8b4d026dff78830f2c2bc44f81713178390c2a
Burning Cactus <23108473+Burning-Cactus@users.noreply.github.com>
2025-05-28 23:50:53 -0500

    Doors work now

--- room/door.gd
+++ room/door.gd
@@ -3,15 +3,5 @@ extends Node2D
 @export var target_room: String
 @export var target_door: String
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_area_2d_body_entered(body: CharacterBody2D) -> void:
-	pass # Replace with function body.
+	RoomManager.switch_room(target_room, target_door)
--- room_manager.gd
+++ room_manager.gd
@@ -1,23 +1,18 @@
 # Singleton script for switching the current room.
 extends Node
 
-var world: Node2D
+@export var world: Node2D
 
 # Key: String ID
-# 
 const rooms: Dictionary = {
-	0: "start",
-	"test1": "test_room_1",
-	"test2": "",
+	"test_1": "test_room_1",
+	"test_2": "test_room_2",
 }
-
-const test_1 := "test_room_1"
-const test_2 := "test_room_2"
 
 
 # Provide the relative path of the room from the scenes folder.
-func switch_room(room_id: String) -> void:
+func switch_room(room_id: String, door_id: String) -> void:
 	assert(world != null, "Error: World is null! Are you trying to switch rooms from outside of the main game?")
 	world.get_children()[0].queue_free()
-	var room := load("res://room/scenes/%s.tscn" % room_id)
+	var room := load("res://room/scenes/%s.tscn" % rooms[room_id])
 	world.add_child(room.instantiate())
