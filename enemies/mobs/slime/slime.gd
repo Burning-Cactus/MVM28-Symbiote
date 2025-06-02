a375b08cba748494d5dc03db32a84adb6d7e6d48
Cheng Her <ckher96@gmail.com>
2025-06-02 14:31:56 -0500

    add idle mode to slime and slime will no longer change directions in the air

--- enemies/mobs/slime/slime.gd
+++ enemies/mobs/slime/slime.gd
@@ -6,7 +6,8 @@ var direction = 1
 var rng = RandomNumberGenerator.new()
 
 @onready var shape_cast: ShapeCast2D = $ShapeCast2D
-@onready var timer: Timer = $Timer
+@onready var jump_timer: Timer = $JumpTimer
+@onready var direction_timer: Timer = $DirectionTimer
 @onready var left_ray_cast: RayCast2D = $LeftRayCast
 @onready var right_ray_cast: RayCast2D = $RightRayCast
 
@@ -14,14 +15,18 @@ var rng = RandomNumberGenerator.new()
 func _process(delta: float):
 	if !is_on_floor():
 		position.x += direction * SPEED * delta
-	if !left_ray_cast.is_colliding(): direction = 1
-	if !right_ray_cast.is_colliding(): direction = -1
-	if shape_cast.is_colliding() && shape_cast.collision_result.any(func(node): return node.collider.name == "Player"):
+	if !left_ray_cast.is_colliding() && is_on_floor(): direction = 1
+	if !right_ray_cast.is_colliding() && is_on_floor(): direction = -1
+	if shape_cast.collision_result.any(func(node): return node.collider.name == "Player"):
 		var collision_results = shape_cast.collision_result
 		var player = collision_results.get(collision_results.find_custom(func(node): return node.collider.name == "Player")).collider
-		SPEED = 80
-		if player.position.x > self.position.x: direction = 1
-		else: direction = -1
+		SPEED = 100
+		direction_timer.paused = true
+		if player.position.x > self.position.x && is_on_floor(): direction = 1
+		if player.position.x < self.position.x && is_on_floor(): direction = -1
+	else: 
+		SPEED = 40
+		direction_timer.paused = false
 		
 func _physics_process(delta: float):
 	if !is_on_floor():
@@ -31,4 +36,9 @@ func _physics_process(delta: float):
 
 func _on_timer_timeout() -> void:
 	velocity.y += jump_strength
-	timer.start(rng.randf() + 1)
+	jump_timer.start(rng.randf() + 1)
+
+
+func _on_direction_timer_timeout() -> void:
+	if is_on_floor():
+		direction *= -1
