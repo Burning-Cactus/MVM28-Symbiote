8229070230ac0f3e052cc848c2e70b16f2b369c8
Cheng Her <ckher96@gmail.com>
2025-06-02 13:54:15 -0500

    Slime will now follow character when entering agro field

--- enemies/mobs/slime/slime.gd
+++ enemies/mobs/slime/slime.gd
@@ -14,14 +14,15 @@ var rng = RandomNumberGenerator.new()
 func _process(delta: float):
 	if !is_on_floor():
 		position.x += direction * SPEED * delta
-	if !left_ray_cast.is_colliding():
-		direction = 1
-	if !right_ray_cast.is_colliding():
-		direction = -1
-	if shape_cast.is_colliding():
+	if !left_ray_cast.is_colliding(): direction = 1
+	if !right_ray_cast.is_colliding(): direction = -1
+	if shape_cast.is_colliding() && shape_cast.collision_result.any(func(node): return node.collider.name == "Player"):
 		var collision_results = shape_cast.collision_result
 		var player = collision_results.get(collision_results.find_custom(func(node): return node.collider.name == "Player")).collider
-		print(player.name)
+		SPEED = 80
+		if player.position.x > self.position.x: direction = 1
+		else: direction = -1
+		
 func _physics_process(delta: float):
 	if !is_on_floor():
 		velocity += get_gravity() * delta
