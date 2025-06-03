79dee8ba295d21772c7096762282ebc212324f61
Cheng Her <ckher96@gmail.com>
2025-06-03 16:01:06 -0500

    rat will now follow player

--- enemies/mobs/rat/rat.gd
+++ enemies/mobs/rat/rat.gd
@@ -6,14 +6,23 @@ var direction = 1
 
 @onready var left_ray_cast: RayCast2D = $LeftRayCast
 @onready var right_ray_cast: RayCast2D = $RightRayCast
+@onready var shape_cast: ShapeCast2D = $ShapeCast2D
+@onready var direction_timer: Timer = $DirectionTimer
 
 func _process(delta: float):
 	position.x += direction * SPEED * delta
-	if !left_ray_cast.is_colliding():
-		direction = 1
-	
-	if !right_ray_cast.is_colliding():
-		direction = -1
+	if !left_ray_cast.is_colliding(): direction = 1
+	if !right_ray_cast.is_colliding(): direction = -1
+	if shape_cast.collision_result.any(func(node): return node.collider.name == "Player"):
+		var collision_results = shape_cast.collision_result
+		var player = collision_results.get(collision_results.find_custom(func(node): return node.collider.name == "Player")).collider
+		SPEED = 40
+		direction_timer.paused = true
+		if player.position.x > self.position.x && is_on_floor(): direction = 1
+		if player.position.x < self.position.x && is_on_floor(): direction = -1
+	else: 
+		SPEED = 20
+		direction_timer.paused = false
 	
 func _physics_process(delta: float):
 	if !is_on_floor():
